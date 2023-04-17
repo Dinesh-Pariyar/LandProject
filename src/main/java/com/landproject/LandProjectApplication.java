e30a64760756ee229ec44d54d88a40da59441745
Dinesh-Pariyar <dineshpariyar1013@gmail.com>
2023-04-17 11:11:38 +0545

    first commit

---
+++ src/main/java/com/landproject/LandProjectApplication.java
@@ -0,0 +1,13 @@
+package com.landproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LandProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/landproject/LandProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.landproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
